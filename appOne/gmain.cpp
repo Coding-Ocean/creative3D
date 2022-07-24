c34f082d516ce60b47417f608afc92baafe74f38
Coding-Ocean <eijo524@gmail.com>
2022-07-24 13:20:17 +0900

    comp

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -17,26 +17,38 @@ void gmain()
     BATCH* sphere = container->batch("sphere");
 
     VECTOR lightPos(1,1,1);
-    float ambient = 1.0f;
-    COLOR diffuse = hsv2rgb(60, 255, 255, 255);
+    float ambient = 0.2f;
+    COLOR diffuse(1,1,0);
     
     SHADER* shader = new LAMBERT;
     shader->setLightPosition(lightPos);
     shader->setLightAmbient(ambient);
     shader->setDiffuse(diffuse);
-    setRasterizerWire();
+    //setRasterizerWire();
     
     MATRIX world;
     MATRIX scale;
     float scl = 0.05f;
-    scale.scaling(scl,scl,scl);
-
+    scale.scaling(scl, scl, scl);
+    unsigned cnt = 0;
+    float speed = 0.05f;
+    float ofst = -0.01f;
+    int sign[4][2] = { 1,1, -1,-1, -1,1, 1,-1 };
     while (notQuit)
     {
         clear(0);
         updateView(shader);
-        world.identity();
-        sphere->draw(shader, world);
+        float s = Sin(cnt * speed);
+        for (int j = 0; j < 4; j++) {
+            world.identity();
+            for (int i = 0; i < 100; i++) {
+                world.mulRotateZ(cnt * speed * 2);
+                world.mulTranslate(
+                    -s*sign[j][0], -s*sign[j][1], ofst * i);
+                sphere->draw(shader, world * scale);
+            }
+        }
+        cnt++;
     }
 
     delete container;
